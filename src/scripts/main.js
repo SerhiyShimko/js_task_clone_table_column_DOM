8c9b5ba4e7a26462c5dcdec2cf09a45aaf02fe33
Serhii Shimko <shimkosergij2010@gmail.com>
2026-02-20 17:45:01 +0200

    completed the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,21 @@
 'use strict';
 
 const table = document.querySelector('table');
-const thead = document.querySelector('thead');
-const tfoot = document.querySelector('tfoot');
 
 for (const tr of table.rows) {
   const row = tr.cells;
   const needTr = row[1];
-  const elementAfter = tr.children[tr.children.length - 1];
+  const clone = needTr.cloneNode(true);
 
-  if (tr.parentNode === thead || tr.parentNode === tfoot) {
-    const newTh = document.createElement('th');
-
-    newTh.textContent = needTr.textContent;
-    elementAfter.before(newTh);
-    continue;
-  } else {
-    const newTd = document.createElement('td');
-
-    newTd.textContent = needTr.textContent;
-    elementAfter.before(newTd);
+  if (!needTr) {
     continue;
   }
+
+  if (tr.children.length === 0) {
+    tr.append(clone);
+  }
+
+  const elementAfter = tr.children[tr.children.length - 1];
+
+  elementAfter.before(clone);
 }
