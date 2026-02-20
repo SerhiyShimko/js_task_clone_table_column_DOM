2f63d905d36a55acfa85217fa7e23bd53c31e9d5
Serhii Shimko <shimkosergij2010@gmail.com>
2026-02-20 17:21:34 +0200

    completed the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+
+for (const tr of table.rows) {
+  const row = tr.cells;
+  const needTr = row[1];
+  const elementAfter = tr.children[tr.children.length - 1];
+
+  if (tr.parentNode === thead || tr.parentNode === tfoot) {
+    const newTh = document.createElement('th');
+
+    newTh.textContent = needTr.textContent;
+    elementAfter.before(newTh);
+    continue;
+  } else {
+    const newTd = document.createElement('td');
+
+    newTd.textContent = needTr.textContent;
+    elementAfter.before(newTd);
+    continue;
+  }
+}
