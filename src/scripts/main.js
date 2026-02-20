eec18d800e993cef790da1ccfcb9626cc4f58fac
Serhii Shimko <shimkosergij2010@gmail.com>
2026-02-20 17:57:15 +0200

    completed the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,31 @@
 'use strict';
 
-const table = document.querySelector('table');
+function cloneRows() {
+  const table = document.querySelector('table');
 
-for (const tr of table.rows) {
-  const row = tr.cells;
-  const needTr = row[1];
-  const clone = needTr.cloneNode(true);
-
-  if (!needTr) {
-    continue;
+  if (!table) {
+    return;
   }
 
-  if (tr.children.length === 0) {
-    tr.append(clone);
+  for (const tr of table.rows) {
+    const row = tr.cells;
+    const needTr = row[1];
+
+    if (!needTr) {
+      continue;
+    }
+
+    const clone = needTr.cloneNode(true);
+
+    if (tr.children.length === 0) {
+      tr.append(clone);
+      continue;
+    }
+
+    const elementAfter = tr.children[tr.children.length - 1];
+
+    elementAfter.before(clone);
   }
-
-  const elementAfter = tr.children[tr.children.length - 1];
-
-  elementAfter.before(clone);
 }
+
+cloneRows();
